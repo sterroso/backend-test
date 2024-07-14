87ae8e3d68a38f9f233e6f89487b363ed2ce706d
Sergio Terroso <sergio.terroso@gmail.com>
2024-07-14 01:13:32 -0600

    Changed nest-cli.json and main.ts files for gRPC microservices.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'products',
+        protoPath: join(__dirname, 'products/product.proto'),
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
